1f85070d6aad8673b4b93e8574793c0fec536b1f
Tim Dresser <tdresser@gmail.com>
2016-07-09 21:11:02 -0400

    url builder

--- main.py
+++ main.py
@@ -3,26 +3,50 @@ import string
 import time
 
 def get_url():
-    price_max = 500000
-    longitude_min = -80.96712875366207
-    longitude_max=-79.67211532592769
-    latitude_min=43.127442225395875
-    latitude_max=43.68614055675253
-    longitude=-80.4928283691406
-    latitude=43.4464569091797
-    current_page=2
+    culture_id=1
+    application_id=1
+    records_per_page=9
+    maximum_results=9
+    property_search_type_id=1
+    transaction_type_id=2
+    storey_range="0-0"
+    bed_range="0-0"
+    bath_range="0-0"
+    longitude_min="-80.5202083587646"
+    longitude_max="-80.46544837951656"
+    latitude_min="43.43343188607223"
+    latitude_max="43.459479374895054"
+    sort_order="A"
+    sort_by=1
+    view_state="l"
+    longitude="-80.4928283691406"
+    latitude="43.4464569091797"
+    current_page=1
+    property_type_group_id=1
 
-    url = "https://www.realtor.ca/Residential/Map.aspx#CultureId=1&ApplicationId=1&RecordsPerPage=9&MaximumResults=9&PropertySearchTypeId=1&TransactionTypeId=2&StoreyRange=0-0&BedRange=0-0&BathRange=0-0&&SortOrder=A&SortBy=1&viewState=l&PropertyTypeGroupID=1"
+    url = "https://www.realtor.ca/Residential/Map.aspx#"
 
     url += string.join([
-        "PriceMax=" + str(price_max),
+        "CultureId=" + str(culture_id),
+        "ApplicationId=" + str(application_id),
+        "RecordsPerPage=" + str(records_per_page),
+        "MaximumResults=" + str(maximum_results),
+        "PropertySearchTypeId=" + str(property_search_type_id),
+        "TransactionTypeId=" + str(transaction_type_id),
+        "StoreyRange=" + str(storey_range),
+        "BedRange=" + str(bed_range),
+        "BathRange=" + str(bath_range),
         "LongitudeMin=" + str(longitude_min),
         "LongitudeMax=" + str(longitude_max),
         "LatitudeMin=" + str(latitude_min),
         "LatitudeMax=" + str(latitude_max),
+        "SortOrder=" + str(sort_order),
+        "SortBy=" + str(sort_by),
+        "viewState=" + str(view_state),
         "Longitude=" + str(longitude),
         "Latitude=" + str(latitude),
-        "CurrentPage=" + str(current_page)
+        "CurrentPage=" + str(current_page),
+        "PropertyTypeGroupID=" + str(property_type_group_id)
     ], "&")
 
     return url
@@ -34,6 +58,7 @@ driver = webdriver.Firefox()
 url = get_url()
 driver.get(url)
 
+# TODO - check how much sleeping is required.
 time.sleep(3)
 source = driver.execute_script("return document.documentElement.outerHTML")
 
