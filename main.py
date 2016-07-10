182c104c613aecac6d7cc123707cfa32fc178ad5
Tim Dresser <tdresser@gmail.com>
2016-07-09 20:27:47 -0400

    new URL

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ import time
 #r = requests.get(get_url)
 #print r
 
-URL="https://www.realtor.ca/Residential/Map.aspx#CultureId=1&ApplicationId=1&RecordsPerPage=9&MaximumResults=9&PropertySearchTypeId=1&PriceMax=500000&TransactionTypeId=2&StoreyRange=0-0&BedRange=0-0&BathRange=0-0&LongitudeMin=-80.96712875366207&LongitudeMax=-79.67211532592769&LatitudeMin=43.127442225395875&LatitudeMax=43.68614055675253&SortOrder=A&SortBy=1&viewState=l&Longitude=-80.4928283691406&Latitude=43.4464569091797&CurrentPage=1&PropertyTypeGroupID=1"
+URL="https://www.realtor.ca/Residential/Map.aspx#CultureId=1&ApplicationId=1&RecordsPerPage=9&MaximumResults=9&PropertySearchTypeId=1&PriceMax=500000&TransactionTypeId=2&StoreyRange=0-0&BedRange=0-0&BathRange=0-0&LongitudeMin=-80.60938644409175&LongitudeMax=-80.02024459838863&LatitudeMin=43.31033384404341&LatitudeMax=43.576056817658994&SortOrder=A&SortBy=1&viewState=l&Longitude=-80.4928283691406&Latitude=43.4464569091797&CurrentPage=1&PropertyTypeGroupID=1"
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
